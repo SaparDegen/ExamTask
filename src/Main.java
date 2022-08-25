c25ace80af9bc5de01d4141ae754a41d6995532b
SaparDegen <110242885+SaparDegen@users.noreply.github.com>
2022-08-25 10:47:13 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -58,5 +58,7 @@ public class Main {
                 System.out.println(groups[i].getClassName() + " Each class must be taught by at least one professor.");
             }
         }
+        System.out.println(Arrays.toString(profs));
+        System.out.println(Arrays.toString(students));
     }
 }
